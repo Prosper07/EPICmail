b166db5a053c8d01e7c6e771bc142c6350f23a17
Prosper07 <prosperkilolo@gmail.com>
2019-03-14 16:34:41 +0200

    removing some non important endpoints

--- server/routes/inbox/routes.js
+++ server/routes/inbox/routes.js
@@ -16,18 +16,18 @@ const router = express.Router();
 // router.use('/api/v1/group', require('../group/post.routes'))
 // router.use('/api/v1/groupmbr', require('../groupmbr/post.routes'))
 
-/* See every inbox message by the admin.. */
-router.get('/api/v1/inbox/', async (req, res) => {
-    await post.getPosts()
-    .then(posts => res.json(posts))
-    .catch(err => {
-        if (err.status) {
-            res.status(err.status).json({ message: err.message })
-        } else {
-            res.status(500).json({ message: err.message })
-        }
-    })
-})
+// /* See every inbox message by the admin.. */
+// router.get('/api/v1/inbox/', async (req, res) => {
+    // await post.getPosts()
+    // .then(posts => res.json(posts))
+    // .catch(err => {
+        // if (err.status) {
+            // res.status(err.status).json({ message: err.message })
+        // } else {
+            // res.status(500).json({ message: err.message })
+        // }
+    // })
+// })
 
 /* See a inbox message by id and message code*/
 router.get('/api/v1/inbox/:id/:pwd', m.mustBeInteger, async (req, res) => {
@@ -45,33 +45,33 @@ router.get('/api/v1/inbox/:id/:pwd', m.mustBeInteger, async (req, res) => {
     })
 })
 
-/* Send message */
-router.post('/api/v1/inbox/', m.checkFieldsPost, async (req, res) => {
-    await post.insertPost(req.body)
-    .then(post => res.status(201).json({
-        message: `201: inbox number ${post.id} has been created`,
-        content: post
-    }))
-    .catch(err => res.status(500).json({ message: err.message }))
-})
+// /* Send message */
+// router.post('/api/v1/inbox/', m.checkFieldsPost, async (req, res) => {
+    // await post.insertPost(req.body)
+    // .then(post => res.status(201).json({
+        // message: `201: inbox number ${post.id} has been created`,
+        // content: post
+    // }))
+    // .catch(err => res.status(500).json({ message: err.message }))
+// })
 
-/* modify message */
-router.put('/api/v1/inbox/:id/:pwd', m.mustBeInteger, m.checkFieldsPost, async (req, res) => {
-    const id = req.params.id
-    const pwd = req.params.pwd
+// /* modify message */
+// router.put('/api/v1/inbox/:id/:pwd', m.mustBeInteger, m.checkFieldsPost, async (req, res) => {
+    // const id = req.params.id
+    // const pwd = req.params.pwd
 
-    await post.updatePost(id, pwd, req.body)
-    .then(post => res.json({
-        message: `201 : Profile information of inbox number ${id} has been updated successfully`,
-        content: post
-    }))
-    .catch(err => {
-        if (err.status) {
-            res.status(err.status).json({ message: err.message })
-        }
-        res.status(500).json({ message: err.message })
-    })
-})
+    // await post.updatePost(id, pwd, req.body)
+    // .then(post => res.json({
+        // message: `201 : Profile information of inbox number ${id} has been updated successfully`,
+        // content: post
+    // }))
+    // .catch(err => {
+        // if (err.status) {
+            // res.status(err.status).json({ message: err.message })
+        // }
+        // res.status(500).json({ message: err.message })
+    // })
+// })
 
 /* Delete one's inbox message */
 router.delete('/api/v1/inbox/:id/:pwd', m.mustBeInteger, async (req, res) => {
--- server/routes/sent/routes.js
+++ server/routes/sent/routes.js
@@ -17,17 +17,17 @@ const router = express.Router();
 // router.use('/api/v1/groupmbr', require('../groupmbr/post.routes'))
 
 /* See every sent message by the admin.. */
-router.get('/api/v1/sent/', async (req, res) => {
-    await post.getPosts()
-    .then(posts => res.json(posts))
-    .catch(err => {
-        if (err.status) {
-            res.status(err.status).json({ message: err.message })
-        } else {
-            res.status(500).json({ message: err.message })
-        }
-    })
-})
+// router.get('/api/v1/sent/', async (req, res) => {
+    // await post.getPosts()
+    // .then(posts => res.json(posts))
+    // .catch(err => {
+        // if (err.status) {
+            // res.status(err.status).json({ message: err.message })
+        // } else {
+            // res.status(500).json({ message: err.message })
+        // }
+    // })
+// })
 
 /* See a sent message by id and message code*/
 router.get('/api/v1/sent/:id/:pwd', m.mustBeInteger, async (req, res) => {
@@ -55,23 +55,23 @@ router.post('/api/v1/sent/', m.checkFieldsPost, async (req, res) => {
     .catch(err => res.status(500).json({ message: err.message }))
 })
 
-/* modify message */
-router.put('/api/v1/sent/:id/:pwd', m.mustBeInteger, m.checkFieldsPost, async (req, res) => {
-    const id = req.params.id
-    const pwd = req.params.pwd
+// /* modify message */
+// router.put('/api/v1/sent/:id/:pwd', m.mustBeInteger, m.checkFieldsPost, async (req, res) => {
+    // const id = req.params.id
+    // const pwd = req.params.pwd
 
-    await post.updatePost(id, pwd, req.body)
-    .then(post => res.json({
-        message: `201 : Profile information of sent number ${id} has been updated successfully`,
-        content: post
-    }))
-    .catch(err => {
-        if (err.status) {
-            res.status(err.status).json({ message: err.message })
-        }
-        res.status(500).json({ message: err.message })
-    })
-})
+    // await post.updatePost(id, pwd, req.body)
+    // .then(post => res.json({
+        // message: `201 : Profile information of sent number ${id} has been updated successfully`,
+        // content: post
+    // }))
+    // .catch(err => {
+        // if (err.status) {
+            // res.status(err.status).json({ message: err.message })
+        // }
+        // res.status(500).json({ message: err.message })
+    // })
+// })
 
 /* Delete one's sent message */
 router.delete('/api/v1/sent/:id/:pwd', m.mustBeInteger, async (req, res) => {
