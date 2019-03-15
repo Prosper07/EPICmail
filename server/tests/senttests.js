bd890d4104e15b31e6d1220f9bed0d00db9688ce
Prosper07 <prosperkilolo@gmail.com>
2019-03-15 17:37:23 +0200

    badges

--- server/tests/senttests.js
+++ server/tests/senttests.js
@@ -31,4 +31,13 @@ chai.use(chaiHttp);
                   expect(res).to.have.property('status')
             });
       });
+      it('it should GET all unread message of user with Id 7 ', () => {
+        chai.request(app)
+            .get('/api/v1/inbox/1/unread')
+            .end((err, res) => {
+              console.log(res.body)
+                  expect(res.body).to.be.an('array')
+                  expect(res).to.have.property('status')
+            });
+      });
   });
