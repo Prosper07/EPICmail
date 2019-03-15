72affa51ed2e6a9639dfd97f74dbb77792313ece
Prosper07 <prosperkilolo@gmail.com>
2019-03-15 17:06:04 +0200

    adding tests

---
+++ server/tests/inboxtests.js
@@ -0,0 +1,34 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+//Import the dev-dependencies
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../server';
+
+const {expect} = chai;
+chai.use(chaiHttp);
+
+/*
+  * Test the /GET route
+  */
+  describe('/GET Users', () => {
+      it('it should GET all messages', () => {
+        chai.request(app)
+            .get('/api/v1/passwordmessages')
+            .end((err, res) => {
+              console.log(res.body)
+                  expect(res.body).to.be.an('array')
+                  expect(res).to.have.property('status')
+            });
+      });
+      it('it should GET user with Id 7 and password 7', () => {
+        chai.request(app)
+            .get('/api/v1/inbox/7/7')
+            .end((err, res) => {
+              console.log(res.body)
+                  expect(res.body).to.be.an('array')
+                  expect(res).to.have.property('status')
+            });
+      });
+  });
---
+++ server/tests/senttests.js
@@ -0,0 +1,25 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+//Import the dev-dependencies
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../server';
+
+const {expect} = chai;
+chai.use(chaiHttp);
+
+/*
+  * Test the /GET route
+  */
+  describe('/GET Users', () => {
+      it('it should GET all messages', () => {
+        chai.request(app)
+            .get('/api/v1/passwordmessages')
+            .end((err, res) => {
+              console.log(res.body)
+                  expect(res.body).to.be.an('array')
+                  expect(res).to.have.property('status')
+            });
+      });
+  });
