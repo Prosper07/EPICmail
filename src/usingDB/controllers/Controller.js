86483356e967349ba3def2943e817612019ec54c
Prosper07 <prosperkilolo@gmail.com>
2019-03-21 10:15:19 +0200

    Finishing validations and hashing

--- src/usingDB/controllers/Controller.js
+++ src/usingDB/controllers/Controller.js
@@ -1,22 +1,36 @@
 import db from '../db';
+import Helper from './Helper';
+import bcrypt from 'bcrypt'
+//import jwt from 'jsonwebtoken';
 
 const Contr = {
   async signup(req, res) {
+    if (!req.body.email || !req.body.password) {
+      return res.status(400).send({'message': 'Both email and password are mendatory'});
+    } else if (!Helper.isValidEmail(req.body.email)) {
+      return res.status(400).send({ 'message': 'Please enter a valid email address' });
+    } else {
+    const hashPassword = Helper.hashPassword(req.body.password);
     const createQuery = `INSERT INTO
     users( email, password) VALUES($1, $2) returning *`;
     const values = [
       req.body.email,
-      req.body.password,
+      hashPassword,
     ];
     db.query(createQuery, values, (err, result) => {
       if(err){
         res.status(500).json({
           status: 500,
-          error : err
+          error : "Internal error or email already exists"
+        })
+      } else {
+      res.json({
+        message : " Congratulation, you now have an EPICmail account!",
+        data:values
         })
       }
-      res.send(result.rows[0])
     });
+    }
   },
 
   async getAll(req, res) {
@@ -29,21 +43,30 @@ const Contr = {
   },
 
   async signin(req, res) {
-    const getOne = 'SELECT * FROM users WHERE email = $1 AND password = $2';
+    if (!req.body.email || !req.body.password) {
+      return res.status(400).send({'message': 'Both email and password are mendatory'});
+    } else if (!Helper.isValidEmail(req.body.email)) {
+      return res.status(400).send({ 'message': 'Please enter a valid email address' });
+    } else {
+    const getOne = 'SELECT * FROM users WHERE email = $1';
     const {email, password} = req.body;
-    const values = [email, password]
-    db.query(getOne, values, (err, result) => {
-      if(err){
-        res.status(500).json({
-          status: 500,
-          error : err
-        })
-      }
-      // if (result.rowCount == 1){ 
-//
-     // }
-      res.send(result.rows[0])
-    });
+    const values = [email, password];
+
+    const {rows}= await db.query(getOne, [req.body.email]);
+    const a = rows[0].password;
+console.log(a);
+const isVerified = bcrypt.compareSync(req.body.password, a)
+if(!isVerified){
+  return res.status(400).send({ 'message': 'The credentials you provided is incorrect' });
+}
+
+     res.json({
+      message : " Welcome to your EPICmail account!",
+      data: rows[0]
+      })
+    
+    
+    }
 }
 }
 
