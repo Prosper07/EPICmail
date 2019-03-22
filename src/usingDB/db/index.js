2a96d848f776b3a8c7d23e2a61ab414edb212a60
Prosper07 <prosperkilolo@gmail.com>
2019-03-22 08:24:43 +0200

    heroku

--- src/usingDB/db/index.js
+++ src/usingDB/db/index.js
@@ -3,11 +3,11 @@ const pg = require('pg');
 const {Pool} = require('pg');
 
 const config = {
-  Host: 'ec2-75-101-131-79.compute-1.amazonaws.com',
-  Database: 'd3kl8aqsvpjoa',
-  User: 'eoifyudvsrleapb', //this is the db user credential
-  Port: 5432,
-  password: '5e5a6878845c8582f0893dfb4e2a08d63dc071d54b7db6885170bf6d9f39235b',
+  host: 'ec2-75-101-131-79.compute-1.amazonaws.com',
+  database: 'pd3kl8aqsvpjoa',
+  user: 'oifyudvsrleapb',
+  port: 5432,
+  password: '5e5a6878845c8582f0893dfb4e2a08d63dc071d54b7db6885170bf6d9f39235b'
 };
 
 const pool = new pg.Pool(config);
