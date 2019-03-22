ee3d60ce2fd1a2d5fb5ee37976580443162a1809
Prosper07 <prosperkilolo@gmail.com>
2019-03-22 09:21:30 +0200

    heroku issues

--- src/usingDB/db/index.js
+++ src/usingDB/db/index.js
@@ -1,16 +1,13 @@
 // src/usingDB/models/index.js
-const pg = require('pg');
-const {Pool} = require('pg');
+import {Pool} from 'pg';
+import dotenv from 'dotenv'; 
 
-const config = {
-  host: 'ec2-75-101-131-79.compute-1.amazonaws.com',
-  database: 'pd3kl8aqsvpjoa',
-  user: 'oifyudvsrleapb',
-  port: 5432,
-  password: '5e5a6878845c8582f0893dfb4e2a08d63dc071d54b7db6885170bf6d9f39235b'
-};
+dotenv.config();
 
-const pool = new pg.Pool(config);
+const pool = new Pool({
+  connectionString : process.env.DATABASE_URL,
+  ssl : true
+});
 
 pool.on('connect', () => {
   console.log('connected to the Database...');
